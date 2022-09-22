306bb5e27ebddf9fa429900c83a31734076deb5e
bendesousa <bdsousa069@gmail.com>
2022-09-23 00:41:59 +0100

    finished car tests

--- car.test.js
+++ car.test.js
@@ -15,3 +15,25 @@ describe("make", () => {
         expect(testcar.setMake("Honda")).toBe("Honda")
     });
 });
+
+describe("price", () => {
+    
+    test("can get car price", () => {
+        expect(testcar.getPrice()).toBe(10000)
+    });
+
+    test("can set car price", () =>{
+        expect(testcar.setPrice(15000)).toBe(15000)
+    });
+});
+
+describe("engineType", () => {
+    
+    test("can get car engineType", () => {
+        expect(testcar.getEngineType()).toBe("Petrol")
+    });
+
+    test("can set car engineType", () =>{
+        expect(testcar.setEngineType("Diesel")).toBe("Diesel")
+    });
+});
