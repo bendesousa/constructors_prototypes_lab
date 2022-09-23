16db2415d92b1b195588e9755f3acf2689afe2f4
bendesousa <bdsousa069@gmail.com>
2022-09-23 08:05:39 +0100

    dealership getters & setters

--- dealership.js
+++ dealership.js
@@ -1,7 +1,13 @@
+const { Car } = require("./car");
+
 const Dealership = function(location, capacity, stock){
     let _location = location;
     let _capacity = capacity;
     let _stock = [];
 
-    
+    this.getLocation = ()=> _location;
+    this.setLocation = (location)=> _location = location;
+    this.getCapacity = ()=> _capacity;
+    this.setCapacity = (capacity)=> _capacity = capacity;
+    this.addToStock = (Car)=> _stock.push(Car);
 }
