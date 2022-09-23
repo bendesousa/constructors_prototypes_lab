d41ca374edaa719a6416da15dc3f01d278596e53
bendesousa <bdsousa069@gmail.com>
2022-09-23 08:44:22 +0100

    linked remote repo

--- dealership.js
+++ dealership.js
@@ -1,6 +1,6 @@
 const { Car } = require("./car");
 
-const Dealership = function(location, capacity, stock){
+const Dealership = function(location, capacity){
     let _location = location;
     let _capacity = capacity;
     let _stock = [];
@@ -10,4 +10,25 @@ const Dealership = function(location, capacity, stock){
     this.getCapacity = ()=> _capacity;
     this.setCapacity = (capacity)=> _capacity = capacity;
     this.addToStock = (Car)=> _stock.push(Car);
-}
+    this.getStock = () => _stock;
+
+    
+    stockCount = 0;
+        for (let i = 0; i < _stock.length; i++){
+        stockCount++;
+        }
+
+
+ 
+};
+
+const dealer1 = new Dealership("North", 100,);
+const testcar = new Car("Toyota", 10000, "Petrol");
+dealer1.addToStock(testcar);
+
+const 
+
+console.log(dealer1.getStock().map((Car) => Car.getMake()));
+console.log(stockCount);
+
+
